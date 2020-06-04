7607c09a946cbc2b95d397676b9bef9a5190186d
Кутюмов Андрей <32129336+Androooooooid@users.noreply.github.com>
2020-06-04 21:08:42 +0400

    work_сut

--- prototypev3/main.cpp
+++ prototypev3/main.cpp
@@ -20,8 +20,6 @@ JOIN-STOCK COMPANY
 
 mysetting myset;
 
-
-
 QScopedPointer<QFile>   m_logFile;
 void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
@@ -39,39 +37,51 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain(ORGANIZATION_DOMAIN);
     QCoreApplication::setApplicationName(APPLICATION_NAME);
     QCoreApplication::setApplicationVersion("3.0");
-
-    QDir dir(myset.getMySetLogPathUser());
-    QFileInfoList st1 = dir.entryInfoList(QDir::NoFilter, QDir::Time);
-//  qDebug()<<st1.size()<<endl<<st1.length();
-    if (st1.length()==2)
-    {
-        QFile file(myset.getMySetLogPathUser()+(QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")+".csv"));
-    }
-    int ind;
-    for (int i=0;st1[i].baseName()=="" ; i++)
-    {
-        qDebug()<<st1[i].baseName();
-        ind=i+1;
-    }
-    QFileInfo f2=st1[2];
-//    if (!QFileInfo fileinfo(f2.absoluteFilePath()));
-//    {
-//        new QFile(myset.getMySetLogPathUser()+(QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")+".csv"));
-//    }
-    QFileInfo fileinfo(f2.absoluteFilePath()); //указываем абсолютный путь к логу к последнему созданому
-    qint64 size = fileinfo.size();
-    qDebug()<<size;
+    qInstallMessageHandler(messageHandler);
     QApplication a(argc, argv);
 
-        qDebug()<<myset.getMySetMaxLogSize()<<size;
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+
+
+
+// Реализация обработчика
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QDir dir(myset.getMySetLogPathUser());
+    QFileInfoList st1 = dir.entryInfoList(QDir::NoFilter, QDir::Time);
+
+    if (st1.length()==2)
+    {
+        m_logFile.reset(new QFile(myset.getMySetLogPathUser()+(QCoreApplication::applicationName())+"_"+(QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")+".csv")));
+        m_logFile.data()->open(QFile::Append | QFile::Text);
+        m_logFile.data()->close();
+        st1 = dir.entryInfoList(QDir::NoFilter, QDir::Time);
+    }
+
+    //qDebug()<<"Начинаем искать последний изменённый лог файл по дате(обладает свойством не пустого baseName) сортированно по времени"<<endl;
+    int ind=0;//Индекс нужного нам лога
+    for (int i=0;st1[i].baseName()=="" ; i++)
+    {
+        ind=i+1;//Изменяем пока не найдём нужный лог
+    }
+
+    QFileInfo f2=st1[ind];//Начинаем работать с нужным нам логом
+    QFileInfo fileinfo(f2.absoluteFilePath()); //указываем абсолютный путь к логу к последнему созданому
+    qint64 size = fileinfo.size();
+
+    //qDebug()<<myset.getMySetMaxLogSize()<<size;
     if (myset.getMySetMaxLogSize()<size)
     {
         // Устанавливаем файл логирования
-        m_logFile.reset(new QFile(myset.getMySetLogPathUser()+(QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")+".csv")));
+        m_logFile.reset(new QFile(myset.getMySetLogPathUser()+(QCoreApplication::applicationName())+"_"+(QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")+".csv")));
         // Открываем файл логирования
         m_logFile.data()->open(QFile::Append | QFile::Text);
         // Устанавливаем обработчик
-        qInstallMessageHandler(messageHandler);
+
     }
     else
     {
@@ -79,19 +89,9 @@ int main(int argc, char *argv[])
         // Открываем файл логирования
         m_logFile.data()->open(QFile::Append | QFile::Text);
         // Устанавливаем обработчик
-        qInstallMessageHandler(messageHandler);
 
     }
-    MainWindow w;
-    w.show();
 
-    return a.exec();
-}
-
-// Реализация обработчика
-void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
-    // Открываем поток записи в файл
     QTextStream out(m_logFile.data());
     // Записываем дату записи
     out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz "",");
@@ -109,4 +109,5 @@ void messageHandler(QtMsgType type, const QMessageLogContext &context, const QSt
         << msg << endl;
     out.flush();    // Очищаем буферизированные данные
 }
+
 
